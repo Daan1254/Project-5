499aa761f81804ae2616b268ffba273a0fffc4c7
Maniusdezeb <daanverbeek15@gmail.com>
2022-06-17 10:36:39 +0200

    React transformt to default HTML css

--- front-end/scripts/login.js
+++ front-end/scripts/login.js
@@ -1,12 +1,24 @@
-export async function tryLogin(username, password, callback) {
-    // let username = user.target.value
-    // let password = pass.target.value
 
+$(".btn").click(() => {
+    let username = $("#login-user").val()
+    let password = $("#login-pass").val()
+
+
+    tryLogin(username, password, (statement) => {
+        if (statement) {
+            notify('succes', "Login Succes", 2500)
+        } else {
+            notify('error', "Login gefaald", 2500)
+        }
+    })
+})
+
+
+const tryLogin = async (username, password, callback) => {
     const response = await fetch("http://node7.consulhosting.nl:24187/login", {
         method: "POST",
         headers: {
             'Content-Type': 'application/json'
-            // 'Content-Type': 'application/x-www-form-urlencoded',
         },
         body: JSON.stringify(
             {
@@ -16,13 +28,9 @@ export async function tryLogin(username, password, callback) {
         )
     })
 
-    let json = await response.json()
     if (response.status === 200) {
         callback(true)
     } else {
         callback(false)
     }
-
-
-
 }
---
+++ front-end/scripts/main.js
@@ -0,0 +1,28 @@
+const notify = (type, message, timeout) => {
+    let x;
+    if (type == 'succes') {
+        x = `<div class="notify ${type}"><i class="far fa-check-circle"></i>${message}<div class="notify-loadingbar loading-${type}"></div></div>`
+        
+    } else if(type == 'error') {
+        x = `<div class="notify ${type}"><i class="fas fa-times"></i>${message}<div class="notify-loadingbar loading-${type}"></div></div>` 
+    } else if (type == 'info') {
+        x = `<div class="notify ${type}"><i class="fas fa-info-circle"></i>${message}<div class="notify-loadingbar loading-${type}"></div></div>` 
+
+    }
+
+    $("body").append(x)
+    $(".notify").animate({
+        right: 2 + "%"
+    }, 1000, () => {
+        document.querySelector(".notify-loadingbar").style.animation = `notify-loadinganim forwards ${timeout}ms`;
+    });
+
+    setTimeout(() => {
+        $(".notify").animate({
+            right: -15 + "%",
+        }, 1000, () => {
+            document.querySelector(".notify-loadingbar").style.animation = ``;
+            $(".notify").remove()
+        }); 
+    }, parseInt(timeout))
+}
