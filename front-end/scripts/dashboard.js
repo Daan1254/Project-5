3df544b929cb6a6f4ad6207badb3df7c22561d51
Tom Keijzers <PS220823@summacollege.nl>
2022-06-21 14:26:46 +0200

    read/delete users

--- front-end/scripts/dashboard.js
+++ front-end/scripts/dashboard.js
@@ -62,6 +62,33 @@ let last_page = "dashboard-home";
 
 let edit_id;
 
+const deleteuser = async (id) => {
+    try {
+            const response = await fetch("http://node7.consulhosting.nl:24187/users/delete/" + id, {
+            method: "DELETE",
+            headers: {
+                'Content-Type': 'application/json',
+                'Accept': 'application/json',
+                'Authorization': acces_token
+            }
+        })
+
+        if (response.status == 200) {
+            notify("succes", "Gebruiker met id " + id + " succesvol verwijderd", 2500)
+            let json = await response.json()
+            acces_token = json.acces_token
+            loadUsers()
+
+        }
+        else {
+            notify("error", "Unauthorized", 2500)
+        }
+    } catch(e) {
+        console.error(e)
+    }
+    
+}
+
 const edit = (id, key) => {
     edit_id = id
     last_page = "dashboard-edit-oefening"
@@ -181,6 +208,37 @@ const sendcreatereq = async () => {
     
 }
 
+let users;
+const loadUsers = async () => {
+    try {
+        const response = await fetch("http://node7.consulhosting.nl:24187/users", {
+            headers: {
+                'Content-Type': 'application/json',
+                'Accept': 'application/json',
+                'Authorization': acces_token
+            }
+        })
+    
+        if (response.status == 200) {
+            const json = await response.json(); 
+            acces_token = json.acces_token
+            users = json.users
+            let x = ''
+            x += `<tr><th>Username</td><th>Email</th><th></th><th></th></tr>`
+            users.forEach((item) => {
+                x += `<tr><td>${item.username}</td><td>${item.email}</td><td style="color: red;" onclick="deleteuser(${item.id})">X</td><td><i class="fas fa-edit"></i></td></tr>`
+            })
+            $(".tabel").html("")
+            $(".tabel").append(x)
+        } else {
+            notify("error", "Unauthorized", 2500)
+        }
+    } catch(e) {
+        console.error(e)
+    }
+    
+}
+
 const goto = (goto) => {
     console.log(goto)
     if (goto == 'home') {
@@ -196,6 +254,13 @@ const goto = (goto) => {
             last_page = "dashboard-oefeningen"
             console.log(last_page)
             loadExercises()
+        })
+    }
+    else if (goto == 'gebruikers') {
+        $(`.${last_page}`).fadeOut(250, () => {
+            $(".dashboard-gebruikers").fadeIn(250)
+            last_page = "dashboard-gebruikers"
+            loadUsers()
         })
     }
 }
